abe329126478aae95723835a404a3ea11a5bebfd
= <=>
2019-07-12 03:00:48 +0300

    heavy update

--- handle-events.js
+++ handle-events.js
@@ -34,14 +34,35 @@
 										                  parel.object3D.scale.x = 0;
 									                  parel.object3D.scale.y = 0;
 									                  parel.object3D.scale.z = 0;
-									              var toel = document.getElementById(el.getAttribute('toid'));
+									              	
+									              	var toel = document.getElementById(el.getAttribute('toid'));
 									  		
-									                  toel.object3D.scale = toel.object3D.original_scale;
+									                  toel.object3D.scale.x = 1;
+
+									                  toel.object3D.scale.y = 1;
+
+									                  toel.object3D.scale.z = 1;
+									                  
 									  	    toel.setAttribute('visible', 'true');
 									              
 									              document.getElementById(el.parentEl.id).setAttribute('visible', 'false');
-									  		  };
-						      		     }
+          									  <!-- change ambient sound -->
+									  var ambient = document.getElementById("ambient_sound");
+									  console.log("getting ambient");
+									  console.log(toel);
+									  var ambient_id = toel.getAttribute('ambient');
+									  ambient.setAttribute('src', document.getElementById(ambient_id).getAttribute('src'));
+									  <!--ambient.setAttribute('src', '#spider_sound');-->
+									  console.log("ambient src");
+									  console.log(ambient.getAttribute("src"));
+
+									  <!--mute all scene sounds>
+								          var sounds = parel.parentEl.querySelectorAll('a-sound');
+									  console.log(parel);
+									  console.log("sounds");
+									  console.log(sounds);
+							  		  };	
+			      		     }
 				      		    
 				      		}
 		            });
